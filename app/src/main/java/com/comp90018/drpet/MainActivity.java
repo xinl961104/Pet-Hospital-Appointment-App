4a50a6eca0a478ac5563f86a4e487ba9cddf15ea
xiandong <dongxian.bruce@gmail.com>
2019-09-25 22:09:58 +1000

    intent to doctor activity

--- app/src/main/java/com/comp90018/drpet/MainActivity.java
+++ app/src/main/java/com/comp90018/drpet/MainActivity.java
@@ -2,9 +2,16 @@ package com.comp90018.drpet;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void goToDoctor(View view) {
+        Intent intent = new Intent(getApplicationContext(), DoctorActivity.class);
+        startActivity(intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
