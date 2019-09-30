dccd08cd4086f262b8f43b3cbb428ae4fabab72d
XIN LI <xinl21@student.unimelb.edu.au>
2019-09-30 17:00:21 +1000

    set hospital,doctor,timeslot javabeans

--- app/src/main/java/com/comp90018/drpet/ChooseDoctorActivity.java
+++ app/src/main/java/com/comp90018/drpet/ChooseDoctorActivity.java
@@ -8,11 +8,18 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class ChooseDoctorActivity extends AppCompatActivity {
-
+    String hospitalName = "Epworth Freemasons";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_doctor);
+        // the name/id of the hospital is from the map sensor.
+        // Here I use a temporary Hospital name which should be replaced after map sensor implemented
+
+
+
+
+
 
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference myRef = database.getReference("message");
---
+++ app/src/main/java/com/comp90018/drpet/DoctorModel.java
@@ -0,0 +1,71 @@
+package com.comp90018.drpet;
+
+/**
+ * @author xinli
+ * @date 2019-09-30
+ */
+public class DoctorModel {
+    private String doctorId;
+    private String doctorFirstName;
+    private String doctorLastName;
+    private String doctorInfo;
+    private String doctorPhone;
+    private String hospitalId;
+
+    public DoctorModel(String doctorId, String doctorFirstName, String doctorLastName, String doctorInfo, String doctorPhone, String hospitalId) {
+        this.doctorId = doctorId;
+        this.doctorFirstName = doctorFirstName;
+        this.doctorLastName = doctorLastName;
+        this.doctorInfo = doctorInfo;
+        this.doctorPhone = doctorPhone;
+        this.hospitalId = hospitalId;
+    }
+
+    public String getDoctorId() {
+        return doctorId;
+    }
+
+    public void setDoctorId(String doctorId) {
+        this.doctorId = doctorId;
+    }
+
+    public String getDoctorFirstName() {
+        return doctorFirstName;
+    }
+
+    public void setDoctorFirstName(String doctorFirstName) {
+        this.doctorFirstName = doctorFirstName;
+    }
+
+    public String getDoctorLastName() {
+        return doctorLastName;
+    }
+
+    public void setDoctorLastName(String doctorLastName) {
+        this.doctorLastName = doctorLastName;
+    }
+
+    public String getDoctorInfo() {
+        return doctorInfo;
+    }
+
+    public void setDoctorInfo(String doctorInfo) {
+        this.doctorInfo = doctorInfo;
+    }
+
+    public String getDoctorPhone() {
+        return doctorPhone;
+    }
+
+    public void setDoctorPhone(String doctorPhone) {
+        this.doctorPhone = doctorPhone;
+    }
+
+    public String getHospitalId() {
+        return hospitalId;
+    }
+
+    public void setHospitalId(String hospitalId) {
+        this.hospitalId = hospitalId;
+    }
+}
