5ed4322bab3a0fa59aa780579ab9485c615388bb
xiandong <dongxian.bruce@gmail.com>
2019-10-03 08:58:47 +1000

    init home

---
+++ app/src/main/java/com/comp90018/drpet/HomeActivity.java
@@ -0,0 +1,14 @@
+package com.comp90018.drpet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
