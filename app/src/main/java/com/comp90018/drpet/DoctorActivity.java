e56d0b457c1f70790773df5dd104bde229c0234a
xiandong <dongxian.bruce@gmail.com>
2019-10-02 18:33:11 +1000

    doctor view

--- app/src/main/java/com/comp90018/drpet/DoctorActivity.java
+++ app/src/main/java/com/comp90018/drpet/DoctorActivity.java
@@ -1,14 +1,22 @@
 package com.comp90018.drpet;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class DoctorActivity extends AppCompatActivity {
 
     TextView hospitalTextView;
+    ImageView hospitalImageView;
+    RecyclerView doctorRecyclerView;
+    TextView aboutTextView;
+    TextView openHoursTextView;
+    Button callButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +24,11 @@ public class DoctorActivity extends AppCompatActivity {
         setContentView(R.layout.activity_doctor);
 
         hospitalTextView = findViewById(R.id.hospitalTextView);
+        hospitalImageView = findViewById(R.id.hospitalImageView);
+        doctorRecyclerView = findViewById(R.id.doctorRecyclerView);
+        aboutTextView = findViewById(R.id.aboutTextView);
+        openHoursTextView = findViewById(R.id.openHoursTextView);
+        callButton = findViewById(R.id.callButton);
 
         Intent intent = getIntent();
         String hospitalName = intent.getStringExtra("hospitalName");
