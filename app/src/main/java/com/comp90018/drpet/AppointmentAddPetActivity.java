cbf0f1a2cb665fd7ba2eb6e9f95de39917bdd916
XIN LI <xinl21@student.unimelb.edu.au>
2019-10-08 02:16:53 +1100

    fix the bug of autoselected

--- app/src/main/java/com/comp90018/drpet/AppointmentAddPetActivity.java
+++ app/src/main/java/com/comp90018/drpet/AppointmentAddPetActivity.java
@@ -1,14 +1,28 @@
 package com.comp90018.drpet;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class AppointmentAddPetActivity extends AppCompatActivity {
+    public String doctorID;
+    public String date;
+    public String time;
+    TextView show;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_appointment_add_pet);
+        Intent intent = getIntent();
+        doctorID = intent.getStringExtra("DoctorIDAppointment");
+        date = intent.getStringExtra("dateforAppointment");
+        time = intent.getStringExtra("selectedTime");
+
+        show = (TextView) findViewById(R.id.textView2);
+        show.setText(doctorID + date + time);
+
     }
 }
--- app/src/main/java/com/comp90018/drpet/TimeSlotActivity.java
+++ app/src/main/java/com/comp90018/drpet/TimeSlotActivity.java
@@ -3,6 +3,7 @@ package com.comp90018.drpet;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
@@ -28,6 +29,9 @@ public class TimeSlotActivity extends AppCompatActivity {
     public TextView Date;
     public Button btnGotoPet;
     public Spinner spinner1;
+    String selecteddate;
+    private boolean isSpinnerInitial = true;
+    String selectedDate;
 
 
     @Override
@@ -44,6 +48,56 @@ public class TimeSlotActivity extends AppCompatActivity {
 
         DoctorID.setText(doctorID);
         Date.setText(date);
+
+        Query queryTimeSlot = FirebaseDatabase.getInstance().getReference("TimeSlot").orderByChild("doctorID").equalTo(doctorID);
+
+        queryTimeSlot.addValueEventListener(new ValueEventListener() {
+            @Override
+
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if(dataSnapshot.exists()){
+                    final List<String> times = new ArrayList<>();
+                    for(DataSnapshot timeSlotSnapShot : dataSnapshot.getChildren()){
+                        TimeSlotModel timeSlot = timeSlotSnapShot.getValue(TimeSlotModel.class);
+                        if(timeSlot.getDate().equals(date) && timeSlot.getFlag().equals("0")){
+                            times.add(timeSlot.getStartTime());
+                        }
+                    }
+
+                    ArrayAdapter<String> adapter =
+                            new ArrayAdapter<String>(getApplicationContext(),  android.R.layout.simple_spinner_dropdown_item, times);
+                    adapter.setDropDownViewResource( android.R.layout.simple_spinner_dropdown_item);
+                    spinner1.setAdapter(adapter);
+
+                    spinner1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                        @Override
+
+                        public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+                                selectedDate = spinner1.getSelectedItem().toString();
+
+                            }
+
+
+
+                        @Override
+                        public void onNothingSelected(AdapterView<?> adapterView) {
+
+                        }
+                    });
+
+
+                }else{
+                    Toast.makeText(TimeSlotActivity.this, "choose other dates, such as 2019/9/17", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
         // go to the next page
         btnGotoPet = (Button)findViewById(R.id.Finally);
 
@@ -53,12 +107,13 @@ public class TimeSlotActivity extends AppCompatActivity {
                 Intent intent = new Intent(TimeSlotActivity.this, AppointmentAddPetActivity.class);
                 intent.putExtra("DoctorIDAppointment", doctorID);
                 intent.putExtra("dateforAppointment",date);
+                intent.putExtra("selectedTime", selectedDate);
                 startActivity(intent);
             }
         });
 
         //
-        loadSpinnerTimeSlot();
+
 
     }
     public void loadSpinnerTimeSlot(){
@@ -82,6 +137,30 @@ public class TimeSlotActivity extends AppCompatActivity {
                     adapter.setDropDownViewResource( android.R.layout.simple_spinner_dropdown_item);
                     spinner1.setAdapter(adapter);
 
+                    spinner1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                        @Override
+
+                        public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                            if (isSpinnerInitial) {
+                                isSpinnerInitial = false;
+                            } else {
+                                String selectedDate = spinner1.getSelectedItem().toString();
+
+                                Intent intent = new Intent(TimeSlotActivity.this, AppointmentAddPetActivity.class);
+                                intent.putExtra("DoctorIDAppointment", doctorID);
+                                intent.putExtra("dateforAppointment",date);
+                                intent.putExtra("selectedTime", selectedDate);
+
+                                startActivity(intent);
+                            }
+                        }
+
+
+                        @Override
+                        public void onNothingSelected(AdapterView<?> adapterView) {
+
+                        }
+                    });
 
 
                 }else{
