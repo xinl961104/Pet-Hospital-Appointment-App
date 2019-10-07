e00156d4f9fe8345a8edc74b704c075554dd511b
XIN LI <xinl21@student.unimelb.edu.au>
2019-10-08 00:35:47 +1100

    add spinners

--- app/src/main/java/com/comp90018/drpet/TimeSlotActivity.java
+++ app/src/main/java/com/comp90018/drpet/TimeSlotActivity.java
@@ -1,14 +1,33 @@
 package com.comp90018.drpet;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class TimeSlotActivity extends AppCompatActivity {
+    String doctorID;
+    String date;
+    public TextView DoctorID;
+    public TextView Date;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_time_slot);
+        Intent intent = getIntent();
+        doctorID = intent.getStringExtra("DoctorIDForPet");
+        date = intent.getStringExtra("DateForPet");
+
+        DoctorID = (TextView) findViewById(R.id.DoctorID);
+        Date =  (TextView) findViewById(R.id.selecteddate);
+
+        DoctorID.setText(doctorID);
+        Date.setText(date);
+
+
+
+
     }
 }
