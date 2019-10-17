36b646ff37157c93f6f3252fa857d9e43f3de956
XIN LI <xinl21@student.unimelb.edu.au>
2019-10-17 18:28:34 +1100

    adding relationship between the user and pet

--- app/src/main/java/com/comp90018/drpet/AppointmentAddPetActivity.java
+++ app/src/main/java/com/comp90018/drpet/AppointmentAddPetActivity.java
@@ -12,6 +12,8 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
@@ -28,7 +30,10 @@ public class AppointmentAddPetActivity extends AppCompatActivity {
     public Spinner spinner2;
     public String selectedPet;
     TextView show;
-
+    public String userEmail;
+    public String userID;
+    public String userName;
+    public String ownerID;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,7 +48,14 @@ public class AppointmentAddPetActivity extends AppCompatActivity {
         spinner2 = (Spinner) findViewById(R.id.PetList);
 
 
-        Query queryPet = FirebaseDatabase.getInstance().getReference("Pet").orderByChild("doctorID");
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if(user!=null){
+            userEmail = user.getEmail();
+            userID = user.getUid();
+            userName = user.getDisplayName();
+        }
+
+        Query queryPet = FirebaseDatabase.getInstance().getReference("Pet").orderByChild("ownerID").equalTo(userID);
 
         queryPet.addValueEventListener(new ValueEventListener() {
             @Override
@@ -89,6 +101,10 @@ public class AppointmentAddPetActivity extends AppCompatActivity {
 
             }
         });
+
+        // the whole booking action is shown here
+
+
 
 
     }
