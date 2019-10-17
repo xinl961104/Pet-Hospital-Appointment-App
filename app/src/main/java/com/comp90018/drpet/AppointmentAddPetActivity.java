4ec7ac7335463f9b3e35b925e1eec9b72da55196
XIN LI <xinl21@student.unimelb.edu.au>
2019-10-18 01:47:40 +1100

    basic booking done

--- app/src/main/java/com/comp90018/drpet/AppointmentAddPetActivity.java
+++ app/src/main/java/com/comp90018/drpet/AppointmentAddPetActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -16,6 +18,7 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
@@ -34,11 +37,21 @@ public class AppointmentAddPetActivity extends AppCompatActivity {
     public String userID;
     public String userName;
     public String ownerID;
+    public String doctorFirstName;
+    public String doctorLastName;
+    public String hospitalId = null;
+    public String hospitalName;
+    Button book;
+    public EditText ownerComment;
+    public String content;
+
+    private DatabaseReference mDatabase;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_appointment_add_pet);
-        Intent intent = getIntent();
+        final Intent intent = getIntent();
         doctorID = intent.getStringExtra("DoctorIDAppointment");
         date = intent.getStringExtra("dateforAppointment");
         time = intent.getStringExtra("selectedTime");
@@ -46,15 +59,56 @@ public class AppointmentAddPetActivity extends AppCompatActivity {
         show = (TextView) findViewById(R.id.textView2);
         show.setText(doctorID + date + time);
         spinner2 = (Spinner) findViewById(R.id.PetList);
+        book = (Button) findViewById(R.id.BookAppointment);
+        ownerComment = (EditText) findViewById(R.id.AddPetComment);
+        content = ownerComment.getText().toString(); //gets you the contents of edit text
 
-
+        // query to correct user for appointment
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
         if(user!=null){
             userEmail = user.getEmail();
             userID = user.getUid();
             userName = user.getDisplayName();
         }
+        // query all doctors' information for the appointment
+        Query queryDoctor = FirebaseDatabase.getInstance().getReference("Doctor").orderByChild("doctorId").equalTo(doctorID);
+        queryDoctor.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if(dataSnapshot.exists()){
+                    for(DataSnapshot DoctorSnapShot:dataSnapshot.getChildren() ){
+                        DoctorModel doctor = DoctorSnapShot.getValue(DoctorModel.class);
+                        doctorFirstName = doctor.getDoctorFirstName();
+                        doctorLastName = doctor.getDoctorLastName();
+                        hospitalId = doctor.getHospitalId();
+                        Query queryHospital = FirebaseDatabase.getInstance().getReference("Hospital").orderByChild("hospitalId").equalTo(hospitalId);
+                        queryHospital.addListenerForSingleValueEvent(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                                for(DataSnapshot HospitalSnapShot: dataSnapshot.getChildren()){
+                                    HospitalModel Hospital = HospitalSnapShot.getValue(HospitalModel.class);
+                                    hospitalName = Hospital.getHospitalName();
+                                }
+                            }
 
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                            }
+                        });
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        //query the important hospital info from the database for the appointment
+
+        // query all pets' from the correct owner from database
         Query queryPet = FirebaseDatabase.getInstance().getReference("Pet").orderByChild("ownerID").equalTo(userID);
 
         queryPet.addValueEventListener(new ValueEventListener() {
@@ -103,9 +157,44 @@ public class AppointmentAddPetActivity extends AppCompatActivity {
         });
 
         // the whole booking action is shown here
+        book.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
+                // add a new row to the appointmentFinal database;
+
+                FirebaseDatabase database = FirebaseDatabase.getInstance();
+                DatabaseReference myRef = database.getReference("AppointmentFinal");
+                String key =  myRef.push().getKey();
+                Appointment app = new Appointment(key, doctorID, userID, "123", selectedPet, content, time, date, "xiandong", userEmail, "booked", doctorFirstName, doctorLastName,hospitalName);
+                myRef.child(key).setValue(app);//this creates the reqs key-value pair
+            //    myRef.child(key).child("title").setValue("Announcing COBOL");//this creates the reqs key-value pair
+
+                // update the timedslot flag in the timeslot table;
+
+
+                Intent appointment = new Intent(AppointmentAddPetActivity.this, ShowBookInfoActivity.class);
+
+                appointment.putExtra("commentFromOwner", content);
+                appointment.putExtra("dateForAppointment", date);
+                appointment.putExtra("DoctorFirstName",doctorFirstName );
+                appointment.putExtra("DoctorLastName",doctorLastName);
+                appointment.putExtra("DoctorId", doctorID);
+                appointment.putExtra("HospitalName", hospitalName);
+                appointment.putExtra("HospitalId",hospitalId);
+                appointment.putExtra("PetName", selectedPet);
+                appointment.putExtra("StartTime", time);
+                appointment.putExtra("UserEmail", userEmail);
+                appointment.putExtra("UserName",userName);
+                appointment.putExtra("UserId", userID);
+
+                startActivity(appointment);
+            }
+        });
 
 
 
     }
+
+
 }
--- app/src/main/java/com/comp90018/drpet/UserModel.java
+++ app/src/main/java/com/comp90018/drpet/UserModel.java
@@ -6,4 +6,37 @@ public class UserModel {
 //    public String imageurl;
     public String name;
     public String email;
+
+    public UserModel(){
+
+    }
+    public UserModel(String uid, String name, String email) {
+        this.uid = uid;
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
